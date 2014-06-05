1bf77e5e130917c86ced61fc6cd0559fd6107330
Ifu Aniemeka <ianiemeka@gmail.com>
2014-06-04 19:59:18 -0500

    Create app file structure sans Rails

---
+++ Gemfile
@@ -0,0 +1,21 @@
+source 'https://rubygems.org'
+ruby '2.1.1'
+
+# Core Application
+gem 'activemodel', '~> 4.1'
+gem 'pg'
+gem 'activerecord', '~> 4.1.0'
+gem 'active_record_tasks', '~> 1.0.3'
+gem 'geocoder'
+gem 'gmaps4rails'
+gem 'omniauth'
+gem 'omniauth-facebook', '1.4.0'
+gem 'dotenv-rails'
+gem 'dotenv-deployment'
+
+# Testing
+gem 'rspec', '~> 2.14.1'
+gem 'pry-debugger', '~> 0.2.2'
+gem 'timecop', '~> 0.7.1'
+gem 'vcr'
+
---
+++ lib/app_helper.rb
@@ -0,0 +1,38 @@
+require 'active_model'
+require 'active_record'
+require 'active_record_tasks'
+require 'pry-debugger'
+require 'yaml'
+require 'time'
+require 'geocoder'
+require 'dotenv-rails'
+
+module RunPal
+  def self.db
+    @db_class ||= Database::InMemory
+    @__db_instance ||= @db_class.new(@env || 'test')
+  end
+
+  def self.db_class=(db_class)
+    @db_class = db_class
+  end
+
+  def self.env=(env_name)
+    @env = env_name
+  end
+
+  def self.db_seed
+    self.db.clear_everything
+    # ADD SEED INFORMATION HERE
+  end
+end
+
+require_relative 'run_pal/entity.rb'
+require_relative 'use_case.rb'
+
+Dir["#{File.dirname(__FILE__)}/run_pal/database/*.rb"].each { |f| require(f) }
+Dir["#{File.dirname(__FILE__)}/run_pal/entities/*.rb"].each { |f| require(f) }
+Dir["#{File.dirname(__FILE__)}/run_pal/use_cases/*.rb"].each { |f| require(f) }
+
+
+

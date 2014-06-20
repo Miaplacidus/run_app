e9cf636689c78b4c685cedf99c9e4608514e5399
Ifu Aniemeka <ianiemeka@gmail.com>
2014-06-20 16:54:10 -0500

    Implement use case to accept join requests

---
+++ lib/run_pal/use_cases/accept_join_request.rb
@@ -0,0 +1,43 @@
+module RunPal
+  class AcceptJoinRequest < UseCase
+
+    def run(inputs)
+
+      inputs[:user_id] = inputs[:user_id].to_i
+      inputs[:join_req_id] = inputs[:join_req_id].to_i
+      inputs[:circle_id] = inputs[:circle_id].to_i
+
+      user = RunPal.db.get_user(inputs[:user_id])
+      return failure(:user_does_not_exist) if user.nil?
+
+      circle = RunPal.db.get_circle(inputs[:circle_id])
+      return failure(:circle_does_not_exist) if circle.nil?
+      return failure (:circle_full) if circle.member_ids.length == circle.max_members
+
+      join_req = RunPal.db.get_join_req(inputs[:join_req_id])
+      return failure(:request_does_not_exist) if join_req.nil?
+
+      return failure (:user_not_authorized) if inputs[:user_id] != circle.admin_id
+
+      join_req = accept_join_req(inputs)
+
+      new_user = RunPal.db.get_user(join_req.user_id)
+      return failure(:new_user_does_not_exist) if new_user.nil?
+
+      inputs[:new_user_id] = new_user.id
+
+      updated_circle = new_circle_member(inputs)
+
+      success :circle => updated_circle, :user => new_user
+    end
+
+    def accept_join_req(attrs)
+      RunPal.db.approve_req(attrs[:join_req_id])
+    end
+
+    def new_circle_member(attrs)
+      RunPal.db.add_user_to_circle(attrs[:circle_id],[attrs[:new_user_id]])
+    end
+
+  end
+end

895324ca748989ac8be8d099e1db1940eeb23900
Ifu Aniemeka <ianiemeka@gmail.com>
2014-06-20 10:22:25 -0500

    Implement use case for creating posts

--- lib/run_pal/use_cases/create_post.rb
+++ lib/run_pal/use_cases/create_post.rb
@@ -5,7 +5,7 @@ module RunPal
 
       inputs[:creator_id] = inputs[:creator_id].to_i
       inputs[:pace] = inputs[:pace].to_i
-      inputs[:time] = inputs[:time].to_i
+      # inputs[:time] = inputs[:time].to a datetime object
       inputs[:min_distance] = inputs[:min_distance].to_i
       inputs[:age_pref] = inputs[:age_pref].to_i
       inputs[:gender_pref] = inputs[:gender_pref].to_i
@@ -14,6 +14,8 @@ module RunPal
 
       user = RunPal.db.get_user(inputs[:creator_id])
       return failure (:user_does_not_exist) if user.nil?
+      return failure (:gender_incorrect) if gender < 1 || gender > 2
+      return failure (:gender_pref_disallowed) if inputs[:user_gender] != inputs[:gender_pref] && inputs[:gender_pref] != 0
 
       post = create_new_post(inputs)
       return failure(:invalid_input) if !post.valid?
@@ -22,6 +24,10 @@ module RunPal
     end
 
     def create_new_post(attrs)
+      attrs.delete_if do |name, value|
+          setter = "#{name}"
+          !RunPal::Post.method_defined?(setter)
+        end
       RunPal.db.create_post(attrs)
     end
 

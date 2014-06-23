85e9b666c8e87eee9ebe377e29aa88f72576a341
Ifu Aniemeka <ianiemeka@gmail.com>
2014-06-23 18:44:31 -0500

    Add notes property to challenge entity

--- lib/run_pal/entities/challenge.rb
+++ lib/run_pal/entities/challenge.rb
@@ -1,6 +1,6 @@
 module RunPal
   class Challenge < Entity
-    attr_accessor :id, :name, :sender_id, :recipient_id, :post_id, :state
+    attr_accessor :id, :name, :sender_id, :recipient_id, :post_id, :state, :notes
     validates_presence_of :name, :sender_id, :recipient_id, :post_id
 
     # sender_id and recipient_id refer to the ids of the sending and
@@ -9,6 +9,7 @@ module RunPal
 
     def initialize(attrs={})
       @state = 'pending'
+      @notes = ''
       super
     end
   end

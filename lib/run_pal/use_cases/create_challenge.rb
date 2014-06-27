0e8099d7bdf5bce01dca9a7478aed287e4f53681
Ifu Aniemeka <ianiemeka@gmail.com>
2014-06-26 22:59:28 -0500

    Implement create challenge use case and associated spec

--- lib/run_pal/use_cases/create_challenge.rb
+++ lib/run_pal/use_cases/create_challenge.rb
@@ -2,12 +2,10 @@ module RunPal
   class CreateChallenge < UseCase
 
     def run(inputs)
-
       # Challenge-specific properties
       inputs[:sender_id] = inputs[:sender_id].to_i
       inputs[:recipient_id] = inputs[:recipient_id].to_i
       inputs[:user_id] = inputs[:user_id].to_i
-
 
       sender = RunPal.db.get_circle(inputs[:sender_id])
       return failure (:circle_does_not_exist) if sender.nil?
---
+++ spec/use_cases/create_challenge_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+describe RunPal::CreateChallenge do
+
+  before :each do
+    RunPal.db.clear_everything
+  end
+
+  it 'creates a new challenge' do
+    user1 = RunPal.db.create_user({username:"Isaac Asimov", gender: 2, email: "isaac@smarty.com"})
+    user2 = RunPal.db.create_user({username:"Karl Asimov", gender: 2, email: "karl@smarty.com"})
+    circle1 = RunPal.db.create_circle({name: "MakerSquare", admin_id: user1.id, max_members: 30, latitude: 33.99, longitude: -9.34, description: "We teach code.", level: -1})
+    circle2 = RunPal.db.create_circle({name: "MassRelevance", admin_id: user2.id, max_members: 30, latitude: -33.49, longitude: -9.22, description: "We ship code", level: -1})
+    post = RunPal.db.create_post({ creator_id: user1.id, max_runners: 10, time: Time.now, pace: 3, notes: "Fun!", min_amt: 12.50, age_pref: 3, gender_pref: 0})
+
+    result = subject.run({name: "Maker-Mass Challenge", sender_id: circle1.id, recipient_id: circle2.id, notes:"Whoop yo ass!", user_id: user1.id})
+    expect(result.success?).to eq(true)
+    expect(result.challenge.name).to eq("Maker-Mass Challenge")
+
+  end
+
+end

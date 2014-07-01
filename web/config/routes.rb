5cbae395dce724b0391281c3ff6be824ebe43bde
Ifu Aniemeka <ianiemeka@gmail.com>
2014-07-01 15:32:44 -0500

    Add Circles controller with associated views

--- web/config/routes.rb
+++ web/config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  get 'circles/index'
+
+  get 'circles/show'
+
+  get 'circles/new'
+
+  get 'circles/create'
+
+  get 'circles/edit'
+
+  get 'circles/delete'
+
+  get 'circles/destroy'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

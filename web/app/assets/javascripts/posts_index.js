318f0a84afe79b265a3eac2aa4cfdcf830cfe767
Ifu Aniemeka <ianiemeka@gmail.com>
2014-07-15 00:07:31 -0500

    Hide search fields unless selected in filter dropdown menu

--- web/app/assets/javascripts/posts_index.js
+++ web/app/assets/javascripts/posts_index.js
@@ -1,5 +1,14 @@
 $(document).ready(function(){
 
+  $('#pace_select, #age_select, #time_select').toggle();
+
+  $('#filter_select').change(function() {
+    var filter = "#" + $("#filter_select option:selected").text() + "_select";
+    filter = filter.toLowerCase();
+    $('#pace_select, #age_select, #time_select').hide();
+    $(filter).toggle();
+  });
+
   $('#radius_select, #gender_select, #pace_select, #age_select').change(function() {
 
   $.ajax({ type: "GET",

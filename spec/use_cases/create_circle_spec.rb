9ff6f426ea848142bab71b9aff2b5b9cb632a597
Ifu Aniemeka <ianiemeka@gmail.com>
2014-07-27 14:34:51 -0500

    Fix use case test for creating a circle

--- spec/use_cases/create_circle_spec.rb
+++ spec/use_cases/create_circle_spec.rb
@@ -8,7 +8,7 @@ describe RunPal::CreateCircle do
 
   it 'creates a new circle' do
     user = RunPal.db.create_user({first_name:"Isaac Asimov", gender: 2, email: "write@smarty.com"})
-    result = subject.run({name: "MakerSquare", latitude: 30.25, longitude: -97.75, user_id: user.id, max_members: 45, description:"We're super fun", level: 3})
+    result = subject.run({name: "MakerSquare", latitude: 30.25, longitude: -97.75, user_id: user.id, max_members: 45, description:"We're super fun", level: 3, city: "Everytown, GA, USA"})
 
     expect(result.success?).to eq(true)
     expect(result.circle.name).to eq("MakerSquare")
--- spec/use_cases/create_post_spec.rb
+++ spec/use_cases/create_post_spec.rb
@@ -10,6 +10,7 @@ describe RunPal::CreatePost do
     user = RunPal.db.create_user({first_name:"Isaac Asimov", gender: 2, email: "write@smarty.com"})
     result = subject.run({latitude:30.25, longitude: -97.75, user_id: user.id, max_runners: 10, time: Time.now, pace: 3, notes: "Fun!", min_amt: 12.50, age_pref: 3, gender_pref: 0, min_distance: 5})
 
+    puts result
     expect(result.success?).to eq(true)
     expect(result.post.max_runners).to eq(10)
     expect(result.post.min_amt).to eq(12.50)

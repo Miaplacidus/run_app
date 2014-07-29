54d212f7495dd7e62d4ce62dafae9270ea58d1bd
Ifu Aniemeka <ianiemeka@gmail.com>
2014-07-29 13:17:12 -0500

    Allow app to access database

--- web/config/initializers/runningbuddy.rb
+++ web/config/initializers/runningbuddy.rb
@@ -1,8 +1,7 @@
-require 'dotenv'
-Dotenv.load
-
 require '../lib/app_helper.rb'
 
+RunPal.db_class = RunPal::Database::ORM
+RunPal.env = 'test'
 RunPal.db_seed
 
 # RunPal.db_class = Timeline::Database::PostGres
